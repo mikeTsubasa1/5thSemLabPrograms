948003a1c6c8a64b36aaf4c405756ce5b2bbe042
tempuname <tempmail>
2017-03-24 16:32:38 +0530

    sample commit

---
+++ sample.py
@@ -0,0 +1,8 @@
+class point:
+    def __init__(self,x=0,y=0):
+        self.x = x
+        self.y = y
+    def print(self):
+        print(self.x,self.y)
+    def __str__(self):
+        return(str(self.x)+" "+str(self.y))
